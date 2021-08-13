35c1a8a34ef153896499fe50d6db042565947912
Michal Trunecka <michal.trunecka@kiwi.com>
2021-08-13 19:40:46 +0200

    Add Terraform for audiozpravy

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "aws-region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "codename" {
+  type    = string
+  default = "audiozpravy"
+}
+
+# Internal domain, its not visible publicly, but it should be domain we own
+# to comply with best practices => it ensures the internal domain names are globally unique.
+variable "codename-domain" {
+  type    = string
+  default = "audiozpravy.ceskodigital.net"
+}
+
+variable "database-username" {
+  type    = string
+  default = "audiozpravydevelopment"
+}
+
+variable "database-password" {
+  type = string
+  default = "heslo"
+}
